67711392b06df8f15007519bf78c8539fb58625f
Shannon Nelson <shannon.nelson@amd.com>
2024-02-09 16:20:02 -0800

    pds_core: no health-thread in VF path
    The VFs don't run the health thread, so don't try to
    stop or restart the non-existent timer or work item.
    
    Fixes: d9407ff11809 ("pds_core: Prevent health thread from running during reset/remove")
    Reviewed-by: Brett Creeley <brett.creeley@amd.com>
    Signed-off-by: Shannon Nelson <shannon.nelson@amd.com>
    Link: https://lore.kernel.org/r/20240210002002.49483-1-shannon.nelson@amd.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/amd/pds_core/main.c
+++ drivers/net/ethernet/amd/pds_core/main.c
@@ -453,6 +453,9 @@ static void pdsc_remove(struct pci_dev *pdev)
 
 static void pdsc_stop_health_thread(struct pdsc *pdsc)
 {
+	if (pdsc->pdev->is_virtfn)
+		return;
+
 	//timer_shutdown_sync(&pdsc->wdtimer);
 	del_timer_sync(&pdsc->wdtimer);
 	if (pdsc->health_work.func)
@@ -461,6 +464,9 @@ static void pdsc_stop_health_thread(struct pdsc *pdsc)
 
 static void pdsc_restart_health_thread(struct pdsc *pdsc)
 {
+	if (pdsc->pdev->is_virtfn)
+		return;
+
 	timer_setup(&pdsc->wdtimer, pdsc_wdtimer_cb, 0);
 	mod_timer(&pdsc->wdtimer, jiffies + 1);
 }
